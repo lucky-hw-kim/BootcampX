4a01bbc6af744852651e4678043850dda1b0f8b4
Lucky Kim <lucky.hw.kim@gmail.com>
2022-04-05 05:46:58 +0000

    Add teacher and assistnace request database

--- 3_queries_group-by/6_low_average_completion_time.sql
+++ 3_queries_group-by/6_low_average_completion_time.sql
@@ -1,3 +1,4 @@
+EXPLAIN
 SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration, AVG(assignments.duration) as average_estimated_duration
 FROM students
 JOIN assignment_submissions ON students.id = student_id
